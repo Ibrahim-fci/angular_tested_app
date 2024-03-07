655b865cc5f627de1e6d690488a981727ea80ad3
Ibrahim-fci <ibrahimismail00000@gmail.com>
2024-03-07 20:29:54 +0200

    feat: (adding app component & transform pipe & auth service) testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,13 +3,6 @@ import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -23,12 +16,7 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('tested_app');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tested_app app is running!');
-  });
+
 
   it('should add two numbers', () => {
     const fixture = TestBed.createComponent(AppComponent);
